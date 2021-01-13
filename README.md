7cb8cf0760918f2a51c351e73859360aada257a8
jehdz <jhernandez23@luc.edu>
2021-01-12 22:07:53 -0600

    express js

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # node-server
+
+Express.js is a powerful and popular tool we can use to create servers.
+
+People use these libraries to make our lives easier
+
+`npm install express`
+
+# How do we initiate an express server? 
+
+`const express = require('express');
+
+const app = express();
+
+app.listen(3000);`
+
+express is really good at routing <br>
+
+express had this concept of middleware. it gets the request of the website, we can do whatever we want with it.
+Then we hit next and express keeps running through the routes <br>
--- server.js
+++ server.js
@@ -1,23 +1,74 @@
-//we grab the http module
-const http = require('http');
-
-const server = http.createServer((request, response) => {
-
-    // console.log('headers', request.headers);
-    console.log('method', request.method);
-    console.log('url', request.url);
-    const user = {
-        name: 'jose',
-        hobby: 'coding'
-    }
-
-    // application/json is how we are able to use json in the server
-    response.setHeader('Content-Type', 'application/json');
-    //this will output a json file on the web
-    response.end(JSON.stringify(user));
-})
-
-//this is the port we want it to listen to
-server.listen(3000);
+// //we grab the http module
+// const http = require('http');
+//
+// const server = http.createServer((request, response) => {
+//
+//     // console.log('headers', request.headers);
+//     console.log('method', request.method);
+//     console.log('url', request.url);
+//     const user = {
+//         name: 'jose',
+//         hobby: 'coding'
+//     }
+//
+//     // application/json is how we are able to use json in the server
+//     response.setHeader('Content-Type', 'application/json');
+//     //this will output a json file on the web
+//     response.end(JSON.stringify(user));
+// })
+//
+// //this is the port we want it to listen to
+// server.listen(3000);
 
 //there are a lot of tools we can use to achive this. A very popular one is Express.js
+
+//this is how we initiate an express server
+
+//the first parameter is the path. req === request | res === response
+//we can use .get .post .delete .post methods
+
+// app.get('/', (req, res) => {
+//     res.send('getting root')
+// });
+//
+// app.get('/profile', (req, res) => {
+//     res.send('getting profile')
+// });
+//
+// app.post('/profile', (req, res) => {
+//     //we can send html or we can send objects
+//     // res.send('<h1> Hellloooooo </h1>')
+//         const user = {
+//         name: 'jose',
+//         hobby: 'coding'
+//     }
+//     res.send(user)
+// });
+
+const express = require('express');
+
+const app = express();
+
+//it gets the request of the website, we can do whatever we want with it.
+// Then we hit next and express keeps running through the routes
+app.use((req, res, next) => {
+    console.log('hello');
+    next();
+})
+
+app.get('/', (req, res) => {
+    res.send('testing')
+});
+
+app.listen(3000);
+
+// its a simple matter of saying
+//if you go to this route, do this, if you go to this other route, do that
+//everything is logical and trickles all the way down
+
+
+
+
+
+
+
